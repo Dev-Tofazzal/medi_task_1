fdd2d7911dc7eab25f3aef780bc14fc88dde68fa
Dev-Tofazzal <dev_tofazzal122@gmail.com>
2024-10-18 11:50:19 +0600

    correction button and pixel perfect

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+      fontFamily:{
+        abc:["Flow Circular", "system-ui"]
+      }
+    },
+    plugins: [],
+  }
